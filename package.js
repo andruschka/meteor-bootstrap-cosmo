ddc56c0d58ca5c50bf5259bd33aabf3341179b77
andrej fritz <a.fritz@betzold.de>
2014-09-10 16:03:16 +0200

    new url

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Provides bootstrap with cosmo theme.",
-  version: "0.0.2",
+  version: "0.0.3",
   git: "https://github.com/andruschka/meteor-bootstrap-cosmo.git"
 });
 
